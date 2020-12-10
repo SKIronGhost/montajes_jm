3b777d951ff5d3ea5d02b7f7221ff91034614e72
SKIronGhost <ed.diegofelix@gmail.com>
2020-12-10 17:09:20 -0300

    cards modified

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
-  $(window).resize(function() {
+  const up1 = $("#up1").html()
+  const down1 = $("#down1").html()
+  const up2 = $("#up2").html()
+  const down2 = $("#down2").html()
+  console.log(up1)
+  console.log(down2)
+  $(window).resize(function () {
   var width = $(window).width();
-  if (width < 1200){
+  if(width < 1200){
     $(".list-group").addClass("text-center");
     $(".exp img").addClass("mx-auto");
   }
+    if (width > 991) {
+      $(".card-service").addClass("h-card")
+      $("#up1").html(down1)
+      $("#down1").html(up1)
+      $("#up2").html(down2)
+      $("#down2").html(up2)
+
+    }
+    else {
+      $(".card-service").removeClass("h-card")
+      $("#up1").html(up1)
+      $("#down1").html(down1)
+      $("#up2").html(up2)
+      $("#down2").html(down2)
+    }  
 })
     
 })
