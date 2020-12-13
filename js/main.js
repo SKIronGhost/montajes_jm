bad0c9c94fac0bcbd69fd7d23cf183363704f2ad
SKIronGhost <ed.diegofelix@gmail.com>
2020-12-13 03:18:07 -0300

    footer with contact done and added some visual modifications

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
   const up1 = $("#up1").html()
   const down1 = $("#down1").html()
   const up2 = $("#up2").html()
   const down2 = $("#down2").html()
-
-  $(window).resize(function () {
   var width = $(window).width();
-  if(width < 1200){
-    $(".list-group").addClass("text-center");
-    $(".exp img").addClass("mx-auto");
-  }
+
+  function services() {
+    width = $(window).width();
     if (width > 991) {
+      $(".sc1").addClass("card-div")
+      $(".sc2").addClass("card-body-service")
       $(".card-service").addClass("h-card")
       $("#up1").html(down1)
       $("#down1").html(up1)
@@ -18,13 +17,24 @@ $(document).ready(function(){
       $("#down2").html(up2)
 
     }
-    else {
+    if (width < 991) {
+      $(".sc1").removeClass("card-div")
+      $(".sc2").removeClass("card-body-service")
       $(".card-service").removeClass("h-card")
       $("#up1").html(up1)
       $("#down1").html(down1)
       $("#up2").html(up2)
       $("#down2").html(down2)
     }  
+  }
+  
+  services()
+  $(window).resize(function () {
+  if(width < 1200){
+    $(".list-group").addClass("text-center");
+    $(".exp img").addClass("mx-auto");
+  }
+  services()
 })
     
 })
